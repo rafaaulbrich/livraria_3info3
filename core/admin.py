1345bc635fdd97c4dd757f610d2ad4585fda4aed
rafaaulbrich <rafaelacamilly2018@gmail.com>
2024-09-09 14:53:21 -0300

    Criação dos itens de compra

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Autor, Categoria, Editora, Livro, Compra, User
+from core.models import Autor, Categoria, Editora, Livro, Compra, ItensCompra, User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
@@ -84,3 +84,4 @@ class LivroAdmin(admin.ModelAdmin):
     list_per_page = 25
 
 admin.site.register(Compra)
+admin.site.register(ItensCompra)
