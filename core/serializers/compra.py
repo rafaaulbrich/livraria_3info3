316647a8d5c3edb79941e276e18f972e4c408df2
rafaaulbrich <rafaelacamilly2018@gmail.com>
2024-09-27 16:07:39 -0300

    mostrando o total do item

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,10 +1,15 @@
-from rest_framework.serializers import CharField, ModelSerializer
+from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
 
 from core.models import Compra, ItensCompra
 class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
+
+    def get_total(self, instance):
+        return instance.livro.preco * instance.quantidade
+    
     class Meta:
         model = ItensCompra
-        fields = ("quantidade", "livro")
+        fields = ("quantidade", "total", "livro")
         depth = 1
 
 class CompraSerializer(ModelSerializer):
